0e278c335feec222fc81c3233b2cb48ac66de1f1
Nora_Chung <k82811@gmail.com>
2025-12-31 15:11:25 +0800

    fix:調整路徑及環境問題

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.PROD ? '/greengo/' : '/',
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-        silenceDeprecations: ['global-builtin', 'import', 'color-functions', 'mixed-decls'],
-        logger: {
-          warn: (message, options) => {
-            // 如果警告訊息包含 "deprecation" 或來自你的 style 資料夾，就直接丟掉不顯示
-            if (options.deprecation) return;
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // 這裡改用 command === 'build' 來判斷
+    base: command === 'build' ? '/greengo/' : '/',
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          silenceDeprecations: ['global-builtin', 'import', 'color-functions', 'mixed-decls'],
+          logger: {
+            warn: (message, options) => {
+              if (options.deprecation) return;
+            },
           },
         },
       },
     },
-  },
+  }
 })
