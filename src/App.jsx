086c001fbc395a1a7863c3df2f9a31af0439e34c
Nora_ccc <k82811@gmail.com>
2026-01-12 19:26:59 +0800

    feat:新增些許api跟token註解

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { Outlet } from "react-router-dom";
 import Header from "./layout/Header.jsx";
 import Footer from "./layout/Footer.jsx";
 
+// 等token確認存到cookie後，請打開這邊的程式碼，將cookie名稱輸入上去
+// const token = document.cookie.split("; ").find((row) => row.startsWith("{存cookie的名稱}"))?.split("=")[1];
+// if (token) {axios.defaults.headers.common['Authorization'] = token}
+
 function App() {
   // 這一段是為了確認，載入axios套件有成功，可以刪除
   useEffect(() => {
