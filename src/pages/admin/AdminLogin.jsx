a8554eed3262f539dcdd1d0a9d65ec62dd2ae79e
YUCCC <qq7002tw@gmail.com>
2026-01-17 16:03:05 +0800

    先推一下

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,29 +1,62 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function AdminLogin() {
+
+    const url = "https://ec-course-api.hexschool.io/v2/";
+    const apiPath = "greengo";
+
+    const [accountLogin, setAccountLogin] = useState("");
+    const [passwordLogin, setPasswordLogin] = useState("");
+    const [token, setToken] = useState("");
+
+    const Login = async () => {
+        try {
+            const res = await axios.post(`${url}admin/signin`, {
+                username: accountLogin,
+                password: passwordLogin
+            });
+            console.log(res);
+            setToken(res.data.token);
+            console.log(token);
+        } catch (error) {
+
+            console.error(error);
+
+        }
+    };
+
+    const getAccount = (e) => {
+        setAccountLogin(e.target.value)
+    }
+
+    const getPassword = (e) => {
+        setPasswordLogin(e.target.value)
+    }
+
+
     return (
         <>
             <main className="mainBody d-flex align-items-center justify-content-center">
 
                 <div className="card maincard py-4">
-                    <div class="card-body d-flex flex-column align-items-center">
-                        <h5 class="card-title text-center mb-4">登入
+                    <div className="card-body d-flex flex-column align-items-center">
+                        <h5 className="card-title text-center mb-4">登入
                         </h5>
-
-
 
                         <div className="col-auto mb-3">
                             <label htmlFor="accountInput" className="form-label">帳號</label>
-                            <input type="email" id="accountInput" className="form-control" placeholder="請輸入帳號" />
+                            <input value={accountLogin}
+                                onChange={getAccount} type="email" id="accountInput" className="form-control" placeholder="請輸入帳號" />
 
                         </div>
-
-
 
                         <div className="col-auto mb-3">
                             <label htmlFor="passwordInput" className="form-label mt-2">密碼</label>
-                            <input type="password" id="passwordInput" className="form-control" placeholder="請輸入密碼" />
+                            <input value={passwordLogin}
+                                onChange={getPassword} type="password" id="passwordInput" className="form-control" placeholder="請輸入密碼" />
                         </div>
-
-                        <a href="#" className="btn btn-primary mt-4 px-3 py-2">送出</a>
+                        <button type="button" className="btn btn-primary mt-4 px-3 py-2 btn_primary_color" onClick={Login}>送出</button>
 
                     </div>
 
