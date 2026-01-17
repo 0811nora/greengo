c0d8ec53ff5f1d784e2588bbd1a8488b5cbbf920
YUCCC <qq7002tw@gmail.com>
2026-01-17 17:37:23 +0800

    串好API

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -4,11 +4,11 @@ import { useState } from "react";
 export default function AdminLogin() {
 
     const url = "https://ec-course-api.hexschool.io/v2/";
-    const apiPath = "greengo";
+
 
     const [accountLogin, setAccountLogin] = useState("");
     const [passwordLogin, setPasswordLogin] = useState("");
-    const [token, setToken] = useState("");
+    const [greenGoToken, setGreenGoToken] = useState("");
 
     const Login = async () => {
         try {
@@ -16,9 +16,9 @@ export default function AdminLogin() {
                 username: accountLogin,
                 password: passwordLogin
             });
-            console.log(res);
-            setToken(res.data.token);
-            console.log(token);
+            // console.log(res);
+            setGreenGoToken(res.data.token);
+            document.cookie = `greenToken=${greenGoToken}; expires=${new Date(res.data.expired)};`;
         } catch (error) {
 
             console.error(error);
@@ -47,16 +47,16 @@ export default function AdminLogin() {
                         <div className="col-auto mb-3">
                             <label htmlFor="accountInput" className="form-label">帳號</label>
                             <input value={accountLogin}
-                                onChange={getAccount} type="email" id="accountInput" className="form-control" placeholder="請輸入帳號" />
+                                onChange={getAccount} type="email" id="accountInput" className="input_login form-control px-4 py-2" placeholder="請輸入帳號" />
 
                         </div>
 
                         <div className="col-auto mb-3">
                             <label htmlFor="passwordInput" className="form-label mt-2">密碼</label>
                             <input value={passwordLogin}
-                                onChange={getPassword} type="password" id="passwordInput" className="form-control" placeholder="請輸入密碼" />
+                                onChange={getPassword} type="password" id="passwordInput" className="form-control input_login  px-4 py-2" placeholder="請輸入密碼" />
                         </div>
-                        <button type="button" className="btn btn-primary mt-4 px-3 py-2 btn_primary_color" onClick={Login}>送出</button>
+                        <button type="button" className="btn btn-primary mt-4 px-5 py-2 btn_login_color" onClick={Login}>送出</button>
 
                     </div>
 
