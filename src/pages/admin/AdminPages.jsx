8005b244961e5775fba198525ddfcac036f5ae6a
soemmaooo <soemmaooo@gmail.com>
2026-01-19 19:02:44 +0800

    feat:商品管理初版

--- src/pages/admin/AdminPages.jsx
+++ src/pages/admin/AdminPages.jsx
@@ -1,55 +1,59 @@
-
 import { Outlet, NavLink } from 'react-router-dom';
-
 
 //登入做好以後刪除
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { admSignin } from "../../api/ApiAdmin";
-
-
+import { admSignin } from '../../api/ApiAdmin';
 
 export default function AdminPages() {
-    
-    //登入做好以後刪除
-    const [isAuth, setIsAuth] = useState(false);
-    const userInfo = {
-        username: "greengo@test.com",
-        password: "12345678"
-    }
-    //登入做好以後刪除
-    useEffect( () => {
-        (async()=>{
-            const res = await admSignin(userInfo);
-            axios.defaults.headers.common['Authorization'] = res.data.token;
-            document.cookie = `greengoToken=${res.data.token};expires=${new Date(res.data.expired)};path=/;`;
-            setIsAuth(true);
-        })()
-    },[])
+  //登入做好以後刪除
+  const [isAuth, setIsAuth] = useState(false);
+  const userInfo = {
+    username: 'greengo@test.com',
+    password: '12345678',
+  };
+  //登入做好以後刪除
+  useEffect(() => {
+    (async () => {
+      const res = await admSignin(userInfo);
+      axios.defaults.headers.common['Authorization'] = res.data.token;
+      document.cookie = `greengoToken=${res.data.token};expires=${new Date(
+        res.data.expired
+      )};path=/;`;
+      setIsAuth(true);
+    })();
+  }, []);
 
-    if (!isAuth) return ;
+  if (!isAuth) return;
 
-
-    return (
-        <main className='adm_bg'>
-            <div className='container'>
-                <div className="row">
-                    <div className="col-2">
-                        <ul>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to='order'>訂單管理</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to='blog'>文章管理</NavLink>
-                            </li>
-
-                        </ul>
-                    </div>
-                    <div className='col-10'>
-                        <Outlet />
-                    </div>
-                </div>
-            </div>
-        </main>
-    )
-};
+  return (
+    <main className="adm_bg">
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <ul>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="order">
+                  訂單管理
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="blog">
+                  文章管理
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="products">
+                  商品管理
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="col-10">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,44 +1,45 @@
-import App from "../App.jsx";
-import { createHashRouter } from "react-router-dom";
-import Home from "../pages/Home.jsx";
-import Product from "../pages/Product.jsx";
-import Custom from "../pages/Custom.jsx";
-import About from "../pages/About.jsx";
-import Article from "../pages/Article.jsx";
-import Header from "../layout/Header.jsx";
-import Footer from "../layout/Footer.jsx";
-import AdminPages from "../pages/admin/AdminPages.jsx";
-import AdminHome from "../pages/admin/AdminHome.jsx";
-import AdminBlog from "../pages/admin/AdminBlog.jsx";
-import AdminOrder from "../pages/admin/AdminOrder.jsx";
-import AdminReport from "../pages/admin/AdminReport.jsx";
-import AdminLogin from "../pages/admin/AdminLogin.jsx";
-
+import App from '../App.jsx';
+import { createHashRouter } from 'react-router-dom';
+import Home from '../pages/Home.jsx';
+import Product from '../pages/Product.jsx';
+import Custom from '../pages/Custom.jsx';
+import About from '../pages/About.jsx';
+import Article from '../pages/Article.jsx';
+import Header from '../layout/Header.jsx';
+import Footer from '../layout/Footer.jsx';
+import AdminPages from '../pages/admin/AdminPages.jsx';
+import AdminHome from '../pages/admin/AdminHome.jsx';
+import AdminBlog from '../pages/admin/AdminBlog.jsx';
+import AdminOrder from '../pages/admin/AdminOrder.jsx';
+import AdminReport from '../pages/admin/AdminReport.jsx';
+import AdminLogin from '../pages/admin/AdminLogin.jsx';
+import AdminProducts from '../pages/admin/AdminProducts.jsx';
 
 const routes = [
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       { index: true, element: <Home /> },
-      { path: "product", element: <Product /> },
-      { path: "about", element: <About /> },
-      { path: "custom", element: <Custom /> },
-      { path: "article", element: <Article /> },
+      { path: 'product', element: <Product /> },
+      { path: 'about', element: <About /> },
+      { path: 'custom', element: <Custom /> },
+      { path: 'article', element: <Article /> },
     ],
   },
   {
-    path: "/admin/login",
+    path: '/admin/login',
     element: <AdminLogin />,
   },
   {
-    path: "/admin",
+    path: '/admin',
     element: <AdminPages />,
     children: [
       { index: true, element: <AdminHome /> },
-      { path: "order", element: <AdminOrder /> },
-      { path: "blog", element: <AdminBlog /> },
-      { path: "report", element: <AdminReport /> },
+      { path: 'order', element: <AdminOrder /> },
+      { path: 'blog', element: <AdminBlog /> },
+      { path: 'report', element: <AdminReport /> },
+      { path: 'products', element: <AdminProducts /> },
     ],
   },
   // {
