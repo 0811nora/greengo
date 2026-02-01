d8036a08f901efc58c9e35ffef26b40315528cb3
YUCCC <qq7002tw@gmail.com>
2026-02-01 20:55:34 +0800

    做一點點

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,8 @@ export default function About() {
         <>
             <main className="container c-about">
                 <div className="topHalfImg">
-                    <h1>GreenGo綠果</h1>
+                    <h1 className="text-center mt-5
+                    ">GreenGo綠果</h1>
                     <img src="../public/img/about_bowl.png" alt="bowl" /></div>
             </main>
         </>
