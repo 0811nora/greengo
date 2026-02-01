4f7d66f665af3156c4e29b57a32991c9b3add359
YUCCC <qq7002tw@gmail.com>
2026-02-01 18:31:59 +0800

    做一半

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,13 @@
+
+
 export default function About() {
     return (
-        <div className="bg-warning text-white">這是關於我們頁</div>
+        <>
+            <main className="container c-about">
+                <div className="topHalfImg">
+                    <h1>GreenGo綠果</h1>
+                    <img src="../public/img/about_bowl.png" alt="bowl" /></div>
+            </main>
+        </>
     )
 }
