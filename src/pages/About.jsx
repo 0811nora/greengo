0a15b79e671418ba09a61af85f98f412407622d7
YUCCC <qq7002tw@gmail.com>
2026-02-03 23:12:18 +0800

    0203進度

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,10 +19,38 @@ export default function About() {
                     <p className="sub-content fs-4" >當你願意從「綠」開始，健康就會在你身上慢慢結「果」。</p>
                     {/* 這邊還沒改完，AOS動畫與transfer衝突 */}
                     <img src="../public/img/about_bowl.png" alt="bowl" /></div>
+                <div>
 
-                <div data-aos="fade-up" data-aos-duration="3000">
-                    <p >第二區塊</p>
+
+                    <div className='second_section position-relative'>
+                        <div className='mid-sec  position-absolute top-0 start-0 py-10'>
+                            <div className='container-fluid'>
+                                <div className='row'>
+                                    <div className='col-md-6'>
+                                        <div className='img-border mt-2 ms-2 mt-md-10 ms-md-10 '>
+                                            <img className='img-fluid mt-md-8 ms-md-9 ' src="../public/img/about_action.png" alt="bowl" />
+                                        </div>
+
+                                    </div>
+                                    {/* 0204從這邊開始寫 */}
+                                    <div className='col-md-6'>
+                                        <h2>行動，然後成果。 The Story of GreenGo</h2>
+                                        <p >GreenGo 的名字，結合了 Green 與 Go／果。Green，代表自然、清新與健康；Go／果，則是你為自己採取行動後，在你身上慢慢開出的成果。</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className='third-sec position-absolute bottom-10 end-0 py-6 px-7'>
+                            <p>因此，GreenGo 想陪你把「行動」變成「成果」。讓你看得懂食物、自在做選擇，為自己的身體負責。</p>
+                            <p>
+                                當你選擇透明、選擇可控、選擇對身體友善，最終收穫的，就是屬於你的健康與自在 ——那就是我們所說的「綠果」。</p>
+                        </div>
+                    </div>
+
                 </div>
+
+
             </main>
         </>
     )
