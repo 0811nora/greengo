2d458a73bc0b21109e464181b0c839144d3112f0
mini <miiinii32ch1129@gmail.com>
2026-02-07 05:42:59 +0800

    feat:篩選邏輯

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,61 +1,63 @@
-import App from '../App.jsx';
-import { createHashRouter, Navigate } from 'react-router-dom';
-import Home from '../pages/Home.jsx';
-import Product from '../pages/Product.jsx';
-import Custom from '../pages/Custom.jsx';
-import About from '../pages/About.jsx';
-import Article from '../pages/Article.jsx';
-import Cart from '../pages/Cart.jsx';
-import Checkout from '../pages/Checkout.jsx';
+import App from "../App.jsx";
+import { createHashRouter, Navigate } from "react-router-dom";
+import Home from "../pages/Home.jsx";
+import Product from "../pages/Product.jsx";
+import Custom from "../pages/Custom.jsx";
+import About from "../pages/About.jsx";
+import Article from "../pages/Article.jsx";
+import Cart from "../pages/Cart.jsx";
+import Checkout from "../pages/Checkout.jsx";
 
-import AdminPages from '../pages/admin/AdminPages.jsx';
-import AdminHome from '../pages/admin/AdminHome.jsx';
-import AdminBlog from '../pages/admin/AdminBlog.jsx';
-import AdminOrder from '../pages/admin/AdminOrder.jsx';
-import AdminReport from '../pages/admin/AdminReport.jsx';
-import AdminLogin from '../pages/admin/AdminLogin.jsx';
-import AdminProducts from '../pages/admin/AdminProducts.jsx';
-import AdminOrder_today from '../pages/admin/AdminOrder_today.jsx';
-import AdminOrder_history from '../pages/admin/AdminOrder_history.jsx';
+import AdminPages from "../pages/admin/AdminPages.jsx";
+import AdminHome from "../pages/admin/AdminHome.jsx";
+import AdminBlog from "../pages/admin/AdminBlog.jsx";
+import AdminOrder from "../pages/admin/AdminOrder.jsx";
+import AdminReport from "../pages/admin/AdminReport.jsx";
+import AdminLogin from "../pages/admin/AdminLogin.jsx";
+import AdminProducts from "../pages/admin/AdminProducts.jsx";
+import AdminOrder_today from "../pages/admin/AdminOrder_today.jsx";
+import AdminOrder_history from "../pages/admin/AdminOrder_history.jsx";
+import { Product2 } from "../pages/Product2.jsx";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'product', element: <Product /> },
-      { path: 'about', element: <About /> },
-      { path: 'custom', element: <Custom /> },
-      { path: 'article', element: <Article /> },
-      { path: 'cart', element: <Cart /> },
-      { path: 'checkout', element: <Checkout /> },
+      { path: "product", element: <Product /> },
+      { path: "product2", element: <Product2 /> },
+      { path: "about", element: <About /> },
+      { path: "custom", element: <Custom /> },
+      { path: "article", element: <Article /> },
+      { path: "cart", element: <Cart /> },
+      { path: "checkout", element: <Checkout /> },
     ],
   },
   {
-    path: '/admin/login',
+    path: "/admin/login",
     element: <AdminLogin />,
   },
   {
-    path: '/admin',
+    path: "/admin",
     element: <AdminPages />,
     children: [
       { index: true, element: <Navigate to="order" replace /> },
       {
-        path: 'order',
+        path: "order",
         element: <AdminOrder />,
         children: [
           {
             index: true,
             element: <Navigate to="today" replace />,
           },
-          { path: 'today', element: <AdminOrder_today /> },
-          { path: 'history', element: <AdminOrder_history /> },
+          { path: "today", element: <AdminOrder_today /> },
+          { path: "history", element: <AdminOrder_history /> },
         ],
       },
-      { path: 'products', element: <AdminProducts /> },
-      { path: 'blog', element: <AdminBlog /> },
-      { path: 'report', element: <AdminReport /> },
+      { path: "products", element: <AdminProducts /> },
+      { path: "blog", element: <AdminBlog /> },
+      { path: "report", element: <AdminReport /> },
     ],
   },
   // {
