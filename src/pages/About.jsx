ca0d7238b2d1348911ea9df2761905218f2c2a99
YUCCC <qq7002tw@gmail.com>
2026-02-07 22:21:20 +0800

    0207更新

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,23 +23,38 @@ export default function About() {
 
 
                     <div className='second_section position-relative'>
-                        <div className='mid-sec  position-absolute top-0 start-0 py-10'>
+                        <div className='mid-sec py-10'>
                             <div className='container-fluid'>
-                                <div className='row'>
+                                <div className='row g-3'>
                                     <div className='col-md-6'>
                                         <div className='img-border d-none d-md-block mt-10 ms-10 '>
-                                            <img className='img-fluid mt-md-8 ms-md-9 ' src="../public/img/about_action.png" alt="bowl" />
+                                            <img className='img-fluid mt-8 ms-9 ' src="../public/img/about_action.png" alt="bowl" />
                                         </div>
 
                                     </div>
 
                                     <div className='col-md-6 mt-10'>
-                                        <div className='d-none d-md-block sub-title fw-semibold fs-2 lh-sm mt-6'>
+                                        <div className='d-none d-md-block sub-title fw-semibold fs-2 lh-sm mt-6 mb-8'>
                                             <h2>行動，<span className='d-block'>然後成果。</span> </h2>
                                             <h3>The Story of GreenGo</h3>
+
                                         </div>
-                                        {/* 0205進度 */}
-                                        <div><p >GreenGo 的名字，結合了 Green 與 Go／果。Green，代表自然、清新與健康；Go／果，則是你為自己採取行動後，在你身上慢慢開出的成果。</p></div>
+                                        <div className='pb-6'>
+                                            <p >GreenGo 的名字，結合了 Green 與 Go／果。</p>
+                                            <p>Green，代表自然、清新與健康；</p>
+
+                                            <p>Go／果，則是你為自己採取行動後，在你身上慢慢開出的成果。</p>
+                                        </div>
+                                        <div className='pb-6'>
+                                            <p>在快節奏的城市裡，外食選擇很多，</p>
+                                            <p>但真正透明、直覺、能讓人安心掌控的選擇，其實不多。</p>
+                                        </div>
+
+                                        <div className='pb-6'>
+                                            <p>GreenGo 的誕生，源自一個簡單卻真實的疑問：</p></div>
+                                        <div>
+                                            <p>「我到底吃了什麼？」</p>
+                                            <p>「我吃的這份餐點，真的適合我嗎？」</p></div>
 
                                     </div>
                                 </div>
@@ -47,9 +62,11 @@ export default function About() {
 
                         </div>
                         <div className='third-sec position-absolute bottom-10 end-0 py-6 px-7'>
-                            <p>因此，GreenGo 想陪你把「行動」變成「成果」。讓你看得懂食物、自在做選擇，為自己的身體負責。</p>
-                            <p>
-                                當你選擇透明、選擇可控、選擇對身體友善，最終收穫的，就是屬於你的健康與自在 ——那就是我們所說的「綠果」。</p>
+                            <p className='pb-6 pe-8'>因此，GreenGo 想陪你把「行動」變成「成果」。<p>讓你看得懂食物、自在做選擇，為自己的身體負責。</p></p>
+
+
+                            <p className=''>
+                                當你選擇透明、選擇可控、選擇對身體友善，<p>最終收穫的，就是屬於你的健康與自在 ——<p>那就是我們所說的「綠果」。</p></p></p>
                         </div>
                     </div>
 
