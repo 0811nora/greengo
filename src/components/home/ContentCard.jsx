2f04eacb00fe5eb8d09a0938bb04622a1b40763a
Chia <angel6160@gmail.com>
2026-02-20 00:25:28 +0800

    fix:首頁施工中

--- src/components/home/ContentCard.jsx
+++ src/components/home/ContentCard.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 const ContentCard = ({
   subTitle, // 英文標題
@@ -8,15 +8,15 @@ const ContentCard = ({
   to, // link
 }) => {
   return (
-    <div className="content-card bg-white shadow">
-      <h4 className="text-gray-200 fs-6 fs-md-4 fw-semibold mb-2 mb-md-5">
+    <div className='content-card bg-white shadow'>
+      <h4 className='text-gray-200 fs-6 fs-md-4 fw-semibold mb-2 mb-md-5'>
         {subTitle}
       </h4>
-      <h2 className="fs-3 fs-md-1 fw-bold mb-2 mb-md-5">{title}</h2>
-      <p className="text-gray-300 mb-2 mb-md-5">{description}</p>
+      <h2 className='fs-3 fs-md-1 fw-bold mb-2 mb-md-5'>{title}</h2>
+      <p className='text-gray-300 mb-2 mb-md-5'>{description}</p>
       <NavLink
         to={to}
-        className="home__btn-primary text-center fw-medium text-decoration-none"
+        className='home__btn-primary text-center fw-medium text-decoration-none mx-auto mx-lg-0'
       >
         {buttonText}
       </NavLink>
