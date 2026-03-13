8dd7b29f6f3bba87547e02b7763ad962b69371fa
Nora_ccc <k82811@gmail.com>
2026-03-13 17:08:35 +0800

    fix:解決合併版本的衝突部分程式碼

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -12,43 +12,6 @@ function ConfirmModal({
 	confirmModal,
 	style,
 }) {
-<<<<<<< HEAD
-  return (
-    <Modal
-      show={show}
-      onHide={closeModal}
-      aria-labelledby="confirmModal"
-      size="lg"
-      className={`confirm-modal ${style}__confirm-modal`}
-      centered
-    >
-      <Modal.Header className="modal__header"></Modal.Header>
-      <Modal.Body className="modal__body">
-        <div className="d-flex justify-content-center">
-          <i className={`${text_icon} modal__icon`}></i>
-        </div>
-        <h1 className="modal__body-title">{text_title}</h1>
-        <p className="modal__body-content">{text_content}</p>
-      </Modal.Body>
-      <Modal.Footer className="d-flex align-items-center gap-2 modal__footer">
-        <button
-          className={`modal__button ${style}__modal__btn-secondary flex-fill`}
-          onClick={cancelModal}
-          type="button"
-        >
-          {text_cancel}
-        </button>
-        <button
-          className={`modal__button ${style}__modal__btn-primary flex-fill`}
-          onClick={confirmModal}
-          type="button"
-        >
-          {text_confirm}
-        </button>
-      </Modal.Footer>
-    </Modal>
-  );
-=======
 	return (
 		<Modal
 			show={show}
@@ -84,7 +47,6 @@ function ConfirmModal({
 			</Modal.Footer>
 		</Modal>
 	);
->>>>>>> origin/main
 }
 
 export { ConfirmModal };
